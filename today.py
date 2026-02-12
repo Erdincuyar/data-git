f0752d4f2203f7de6c7e504a63194b3d8155ddd5
Erdinç Uyar <erdincuyar@192.168.1.52>
2026-02-12 23:26:02 +0300

    Fix style issues, should get a 'Good Style' now :pray:

--- today.py
+++ today.py
@@ -1,4 +1,7 @@
+"""A module computing buddy pair names for the day"""
 def my_name_is():
+    """A module computing buddy pair names for the day"""
     return "Erdincuyar"
 def my_buddy_is():
+    """A module computing buddy pair names for the day"""
     return "Cankel"
